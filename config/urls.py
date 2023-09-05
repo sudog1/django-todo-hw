4614d9677aef1f53f90bbc3cdbfe1c051cd48d8d
sudog <sjin0704@gmail.com>
2023-09-05 22:40:50 +0900

    update config settings and url pattern

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('todo.urls', namespace='todo')),
-    path('user/', include('user.urls', namespace='user')),
+    path("admin/", admin.site.urls),
+    path("", include("todo.urls", namespace="todo")),
+    path("user/", include("user.urls", namespace="user")),
 ]
