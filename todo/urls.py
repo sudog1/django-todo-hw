bc8d66ab8dd6062e2646f8fb4042dd6e1688ada2
sudog <sjin0704@gmail.com>
2023-09-05 20:04:37 +0900

    update todo/urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = "todo"
+
 urlpatterns = [
-    path('', views.home_view),
-    path('todo/', views.todo_view),
-    path('todo/read/<int:id>', views.read_view),
-    path('todo/create/', views.create_view),
-    path('todo/update/<int:id>', views.update_view),
-    path('todo/delete/<int:id>', views.delete_view),
-    path('todo/mypage/', views.mypage_view),
+    path("", views.home_view, name="home"),
+    path("detail/<int:id>", views.detail_view, name="detail"),
+    path("create/", views.create_view, name="create"),
+    path("update/<int:id>", views.update_view, name="update"),
+    path("delete/<int:id>", views.delete_view, name="delete"),
+    path("mypage/", views.mypage_view, name="mypage"),
 ]
