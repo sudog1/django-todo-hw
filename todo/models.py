b4e0bea732a7d2233ad5b76ae997242a06ce6dfd
sudog <sjin0704@gmail.com>
2023-09-06 22:30:51 +0900

    add comment model

--- todo/models.py
+++ todo/models.py
@@ -3,9 +3,23 @@ from user.models import UserModel
 
 
 class TodoModel(models.Model):
-    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    title = models.CharField(max_length=32)
-    content = models.CharField(max_length=256, default="")
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name="todo")
+    title = models.CharField(max_length=64)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_completed = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
+    likes = models.ManyToManyField(UserModel, related_name="likes_todo", blank=True)
+
+
+class CommentModel(models.Model):
+    author = models.ForeignKey(
+        UserModel, on_delete=models.CASCADE, related_name="comment"
+    )
+    todo = models.ForeignKey(
+        TodoModel, on_delete=models.CASCADE, related_name="comment"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(UserModel, related_name="likes_comment", blank=True)
