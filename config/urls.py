ce37a92f979100893c3e2af4bb5c9d4e7efb4e37
sudog <sjin0704@gmail.com>
2023-09-06 22:30:18 +0900

    add media for image

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("todo.urls", namespace="todo")),
     path("user/", include("user.urls", namespace="user")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
